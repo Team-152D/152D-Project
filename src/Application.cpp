6a888bec5c53e68b8569c984b9a975235945f199
Sammy Yuk <dlvg92@mail.csufresno.edu>
2014-03-03 21:19:56 -0800

    Attempting to get multiplayer working

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,7 @@
 #include "Application.h"
 #include "Settings.h"
 #include "Game.h"
+#include <fstream>
 
 Application::Application( )
 {
@@ -39,17 +40,38 @@ int Application::runApplication( )
 				game = NULL;
 				break;
 			case Global::AS_GAME_CONT:
-				game = new Game( false,0 );
-				appState = game->runGame();
+				game = new Game( false, 0 );
+				appState = game->runGame( );
 				delete game;
 				game = NULL;
 				break;
 			case Global::AS_GAME_JOIN:
-				appState = Global::AS_MAIN_MENU;
+			{
+				ifstream infile;
+				infile.open( "rsc\\data\\data_ip.txt" );
+				char IP[16];
+				infile >> IP;
+				cout << "IP is: " << IP << endl;
+				int socket = client( IP );
+				game = new Game( false, socket );
+				appState = game -> runGame( );
+				delete game;
 				break;
+			}
+			case Global::AS_GAME_HOST:
+			{
+				SDL_Surface *blk = image->loadImage( "rsc\\ui\\ui_blackScreen.bmp" );
+				image->drawSurface( 0, 0, blk );
+				text->writeText( 0, 0, "waiting for other player", 44 );
+				int sock = server( );
+				game = new Game( false, sock );
+				appState = game -> runGame( );
+				delete game;
+				break;
+			}
 			case Global::AS_EDITOR:
-				editor = new Editor();
-				appState = editor -> runEditor();
+				editor = new Editor( );
+				appState = editor -> runEditor( );
 				delete editor;
 				editor = NULL;
 				break;
