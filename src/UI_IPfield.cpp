6ab3f2740c9f88e84934904c1d2b2de698d3b1d0
RawrWolfie <bbanevedes@mail.fresnostate.edu>
2014-03-31 18:39:04 -0700

    IP field menu element added.  Testing & review required.  Yet to add image/.txt/ip storage for field.

---
+++ src/UI_IPfield.cpp
@@ -0,0 +1,48 @@
+#include "UI_IPfield.h"
+
+UI_IPfield::UI_IPfield(SDL_Rect fieldSize, int textSize, bool center){
+    addr= ""; mousedown= focus= false; centered= center;
+    fontSize= textSize; bounds= fieldSize;
+    bg= image->loadImage("");
+    blink.start();
+}
+
+int UI_IPfield::handleEvents(SDL_Event* ev){
+    if(ev->type== SDL_KEYDOWN && focus){
+        char k= ev->key.keysym.sym;
+        if(isdigit(k) || k== '.')
+            addr.push_back(k);
+        else if(k== '\b') addr.pop_back();       
+    } else{
+        int x, y;
+        SDL_GetMouseState(&x, &y);
+        if(coordinatesAreInRect(x, y, bounds)){
+                if(ev->type== SDL_MOUSEBUTTONDOWN) mousedown= true;
+                else if(ev->type== SDL_MOUSEBUTTONUP && mousedown){
+                    mousedown= false; focus= true;
+                }
+        } 
+        else if(ev->type== SDL_MOUSEBUTTONDOWN) focus= false;
+        else if(ev->type== SDL_MOUSEBUTTONUP) mousedown= false;        
+    }
+}
+
+void UI_IPfield::draw(){
+    //note the src background must operate with increments of 10pixels (x-axis))
+    for ( int i = bounds.x; i < ( bounds.x + bounds.w ); i += 10 )
+        image->drawSurface(i, bounds.y, bg);
+    cursorBlink();
+    if( centered )
+	text->writeTextCentered(bounds,addr.c_str(),fontSize);
+    else
+	text->writeText(bounds.x,bounds.y,addr.c_str(),fontSize);
+}
+
+void UI_IPfield::cursorBlink(){
+    if(!focus) return;   
+    int t= blink.get_ticks()/1000;
+    if(t%2 && addr.back()!='|')
+        addr.push_back('|');
+    else if(!(t%2) && addr.back()=='|')
+        addr.pop_back();
+}
---
+++ src/UI_IPfield.h
@@ -0,0 +1,33 @@
+/* 
+ * File:   UI_TextField.h
+ * Author: Blaize
+ *
+ * Created on March 24, 2014, 4:11 PM
+ */
+
+#ifndef UI_IPFIELD_H
+#define	UI_IPFIELD_H
+
+#include "UI_AbstractElement.h"
+#include "Text.h"
+#include "Timer.h"
+
+class UI_IPfield: public UI_AbstractElement{
+    public:
+        UI_IPfield(SDL_Rect, int, bool);
+        virtual void draw();
+        virtual int handleEvents(SDL_Event*);
+    private:
+        string addr;//ip address stored by user
+        SDL_Surface* bg;//background
+        bool focus, mousedown, centered;         
+        int fontSize;
+        
+        //currently uses internal timer
+        //consider switch to passed down timer (gametimer)
+        void cursorBlink();
+        Timer blink;
+};
+
+#endif	/* UI_TEXTFIELD_H */
+
