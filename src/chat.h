d935b8ee3a26f96d5ec93be7e08f7e557bb7495a
Matt <mattmoo_13@yahoo.com>
2014-04-30 15:37:35 -0700

    added a way to set player name and input

--- src/chat.h
+++ src/chat.h
@@ -3,11 +3,9 @@
 #ifndef CHAT_H
 #define	CHAT_H
 
-//http://lazyfoo.net/SDL_tutorials/lesson23/index.php
-
 #include <iostream>
 #include <cstdlib>
-#include <sstream>
+//#include <sstream>
 #include "SDL2/SDL.h"
 #include <vector>
 #include "Text.h"
@@ -19,27 +17,18 @@ using namespace std;
 
        chat();
        ~chat();
-       
-       
-       void run(); 
-       
-       
-       //move to private later
        void old_chatting(SDL_Event event); //SDL_Event* event
-      // void chatting();
-       void show_message(string str);
-       void send_message(string str);
-       void show_result();
-       
+       void chatting();
+       void show_result(); 
       // void chatting2();
-      
        void can_type(bool x);
-       
        void break_down(int size);
+       void set_player(string str);
+       void set_string(string str);
        void clear_vect();
        string get_input();
        string get_player();
-       
+       void merge_player_input();
        string get_vect(int x);
        int get_vect_end();
        //Future methods
@@ -51,8 +40,9 @@ using namespace std;
    private: 
        SDL_Surface * inbox, * textbox ; //input box, output box
        bool isTyping; //0 = not typing, 1 = typing
+       int count; //initial state for SDL_StopTextInput()
        string s, player;
-       stringstream in, out; //input and output 
+       //stringstream in, out; //input and output 
        const Uint8 * keystate; //state of keyboard , note: need to include const
        vector<string> input; 
    }; 
